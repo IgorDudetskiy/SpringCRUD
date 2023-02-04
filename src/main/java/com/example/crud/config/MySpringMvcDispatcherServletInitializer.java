070364a23c31fc190b4be9919ce5a04ef60d6d7b
Igor Dudetskiy <116377166+IgorDudetskiy@users.noreply.github.com>
2023-02-04 22:45:31 +0200

    2/4/2023 22:45

---
+++ src/main/java/com/example/crud/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,36 @@
+package com.example.crud.config;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    //onStartup запускается при старте спринг приложения, и запускает registerHiddenFieldFilter() и применяем один фильтр
+    //HiddenHttpMethodFilter() - этот фильтр смотрит на значение скрытого поля
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+}
